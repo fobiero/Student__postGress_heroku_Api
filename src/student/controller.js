ac51b2dd0db7c79b7fe40fd3d46db6048e46a1fd
feLycks <felix.obiero@student.moringaschool.com>
2021-11-24 03:50:28 +0300

    get studentById

--- src/student/controller.js
+++ src/student/controller.js
@@ -1,13 +1,23 @@
 const pool = require('../../db')
+const queries = require('./queries')
 
 const getStudents = (req, res) => {
     // console.log('getting students');
-    pool.query("SELECT * FROM students", (error, results) => {
+    pool.query(queries.getStudents, (error, results) => {
         if (error) throw error;
         res.status(200).json(results.rows);
     })
 }
 
+const getStudentById = (req, res) => {
+    const id = parseInt(req.params.id);
+    pool.query(queries.getStudentById, [id], (error, results) => {
+        if (error) throw error;
+        res.status(200).json(results.rows)
+    })
+}
+
 module.exports = {
     getStudents,
+    getStudentById,
 }
--- src/student/routes.js
+++ src/student/routes.js
@@ -3,6 +3,7 @@ const router = Router();
 
 const controller = require('./controller')
 
-router.get('/', controller.getStudents)
+router.get('/', controller.getStudents);
+router.get('/:id', controller.getStudentById);
 
 module.exports = router;
