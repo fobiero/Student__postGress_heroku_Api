8d10df7501037a9e1ab212d5e0e565aacd34afba
feLycks <felix.obiero@student.moringaschool.com>
2021-11-24 04:16:23 +0300

    create student route

--- src/student/controller.js
+++ src/student/controller.js
@@ -17,7 +17,25 @@ const getStudentById = (req, res) => {
     })
 }
 
+const addStudent = (req, res) => {
+    const { name, email, age, dob } = req.body;
+    // check if email exist 
+
+    pool.query(queries.checkEmailExist, [email], (error, results) => {
+        if (results.rows.length) {
+            res.send('email already exist')
+        }
+
+        // add student to db 
+        pool.query(queries.addStudent, [name, email, age, dob], (error, results) => {
+            if (error) throw error;
+            res.status(201).send('Student created successfully!');
+        })
+    })
+}
+
 module.exports = {
     getStudents,
     getStudentById,
+    addStudent,
 }
--- src/student/routes.js
+++ src/student/routes.js
@@ -4,6 +4,8 @@ const router = Router();
 const controller = require('./controller')
 
 router.get('/', controller.getStudents);
+router.post('/', controller.addStudent);
 router.get('/:id', controller.getStudentById);
+
 
 module.exports = router;
