3c55f8cbf9ccd8da51b8e664db1cc9d0c9ab5986
feLycks <felix.obiero@student.moringaschool.com>
2021-11-24 05:07:51 +0300

    update route finish build

--- src/student/controller.js
+++ src/student/controller.js
@@ -49,9 +49,30 @@ const removeStudent = (req, res) => {
     });
 };
 
+
+const updateStudent = (req, res) => {
+    const id = parseInt(req.params.id);
+    const { name } = req.body;
+
+    // check student if exist 
+    pool.query(queries.getStudentById, [id], (error, results) => {
+        const noStudentFound = !results.rows.length;
+        if (noStudentFound) {
+            res.send('Student not Found');
+        }
+
+        // update student if exists 
+        pool.query(queries.updateStudent, [name, id], (error, results) => {
+            if (error) throw error;
+            res.status(200).send('Student sucessfuly updates!')
+        });
+    });
+
+}
 module.exports = {
     getStudents,
     getStudentById,
     addStudent,
     removeStudent,
+    updateStudent,
 }
